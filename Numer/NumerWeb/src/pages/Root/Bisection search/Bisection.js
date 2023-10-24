78ea24b84388abcf0346acc78cc93b9d82988ffc
focuspaphawarin <paphawarin.focus@gmail.com>
2023-10-25 02:20:30 +0700

    study input

--- Numer/NumerWeb/src/pages/Root/Bisection search/Bisection.js
+++ Numer/NumerWeb/src/pages/Root/Bisection search/Bisection.js
@@ -1,13 +1,128 @@
+// import React from 'react'
+// import Nav from '../../../components/navbar'
+// function Bisection() {
+//     return (
+//         <div>
+//             <Nav />
+//             <div>
+//                 ใบ
+//             </div>
+//         </div>
+//     )
+// }
+// export default Bisection
+import { render } from "@testing-library/react";
 import React from 'react'
-import Nav from '../../../components/navbar'
-function Bisection() {
-    return (
-        <div>
-            <Nav />
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+const Parser = require('expr-eval').Parser;
+
+
+class Bisection extends React.Component
+{
+    constructor(props)
+    {
+        super(props);
+        this.state = {XL:'',func:''}
+        this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)    
+    }
+
+    BisectionCalcFunction(XL,Funct)
+    {
+        const parser = new Parser();
+        function fx(x)
+        {
+            let expr = parser.parse(Funct)
+            console.log("fx = "+expr.evaluate({ x: (x) }))
+            return expr.evaluate({ x: (x) })
+        }
+      var xl = parseInt(XL);
+      var arr = []
+      var mz
+      var i;
+      for(i=1;i<=xl;i++){
+        mz = fx(i)
+        arr[i] = ({x:i,y:mz})
+        //render(arr[i].x+": "+arr[i].y)
+      }
+      console.log(arr);
+      render(
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 300 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell align="center">X</TableCell>
+              <TableCell align="center">Y</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {arr.map((arr) => (
+              <TableRow
+                key={arr.x}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              >
+                <TableCell component="th" scope="row" align="center">
+                  {arr.x}
+                </TableCell>
+                <TableCell align="center">{arr.y}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      )
+
+    }
+    handleSubmit(event){
+        const {XL,Funct} = this.state
+        const xm = this.BisectionCalcFunction(XL,Funct)
+        event.preventDefault()
+    }
+    handleChange(event){
+      this.setState({[event.target.name] : event.target.value});
+    }
+    render(){
+        return(
+          <div>
+            <form onSubmit={this.handleSubmit}>
             <div>
-                ใบ
+                <h1 className="h1">&emsp;Bisection Method&emsp;</h1>
+              <label htmlFor='XL' className="XLtext">&emsp;XL :&emsp;</label>
+              <input
+                className="XLfield"
+                name='XL'
+                placeholder='XL'
+                value = {this.state.XL}
+                onChange={this.handleChange}
+                size='16'
+              />
+              </div>
+              <p></p>
+              <div>
+              <label htmlFor='Funct' className="Functext">&emsp;Funct :&emsp;</label>
+              <input
+                className="Funcfield"
+                name='Funct'
+                placeholder='FUNCTION'
+                value={this.state.Funct}
+                onChange={this.handleChange}
+                size='30'
+              />
             </div>
-        </div>
-    )
-}
+            <p></p>
+            <div>
+            &emsp;<button className="hbutton">Calculate</button>
+            </div>
+          </form>
+          </div>
+        )
+      }
+    }
+
 export default Bisection
