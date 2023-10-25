5f06689e05b11e56a5ee8dbbe55333100aa4e684
focuspaphawarin <paphawarin.focus@gmail.com>
2023-10-25 20:22:13 +0700

    html input

--- Numer/NumerWeb/src/pages/Root/Bisection search/Bisection.js
+++ Numer/NumerWeb/src/pages/Root/Bisection search/Bisection.js
@@ -11,118 +11,159 @@
 //     )
 // }
 // export default Bisection
-import { render } from "@testing-library/react";
-import React from 'react'
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-const Parser = require('expr-eval').Parser;
+import { useState } from "react"
+import { Button, Container, Form, Table } from "react-bootstrap";
+import { evaluate } from 'mathjs'
 
-
-class Bisection extends React.Component
-{
-    constructor(props)
-    {
-        super(props);
-        this.state = {XL:'',func:''}
-        this.handleChange = this.handleChange.bind(this)
-        this.handleSubmit = this.handleSubmit.bind(this)    
-    }
-
-    BisectionCalcFunction(XL,Funct)
-    {
-        const parser = new Parser();
-        function fx(x)
-        {
-            let expr = parser.parse(Funct)
-            console.log("fx = "+expr.evaluate({ x: (x) }))
-            return expr.evaluate({ x: (x) })
-        }
-      var xl = parseInt(XL);
-      var arr = []
-      var mz
-      var i;
-      for(i=1;i<=xl;i++){
-        mz = fx(i)
-        arr[i] = ({x:i,y:mz})
-        //render(arr[i].x+": "+arr[i].y)
-      }
-      console.log(arr);
-      render(
-      <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 300 }} aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell align="center">X</TableCell>
-              <TableCell align="center">Y</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {arr.map((arr) => (
-              <TableRow
-                key={arr.x}
-                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-              >
-                <TableCell component="th" scope="row" align="center">
-                  {arr.x}
-                </TableCell>
-                <TableCell align="center">{arr.y}</TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-      )
-
-    }
-    handleSubmit(event){
-        const {XL,Funct} = this.state
-        const xm = this.BisectionCalcFunction(XL,Funct)
-        event.preventDefault()
-    }
-    handleChange(event){
-      this.setState({[event.target.name] : event.target.value});
-    }
-    render(){
+// component sample
+const Sample =()=>{
+    const print = () =>{
+        console.log(data)
+        setValueIter(data.map((x)=>x.iteration));
+        setValueXl(data.map((x)=>x.Xl));
+        setValueXm(data.map((x)=>x.Xm));
+        setValueXr(data.map((x)=>x.Xr));
         return(
-          <div>
-            <form onSubmit={this.handleSubmit}>
-            <div>
-                <h1 className="h1">&emsp;Bisection Method&emsp;</h1>
-              <label htmlFor='XL' className="XLtext">&emsp;XL :&emsp;</label>
-              <input
-                className="XLfield"
-                name='XL'
-                placeholder='XL'
-                value = {this.state.XL}
-                onChange={this.handleChange}
-                size='16'
-              />
-              </div>
-              <p></p>
-              <div>
-              <label htmlFor='Funct' className="Functext">&emsp;Funct :&emsp;</label>
-              <input
-                className="Funcfield"
-                name='Funct'
-                placeholder='FUNCTION'
-                value={this.state.Funct}
-                onChange={this.handleChange}
-                size='30'
-              />
-            </div>
-            <p></p>
-            <div>
-            &emsp;<button className="hbutton">Calculate</button>
-            </div>
-          </form>
-          </div>
-        )
-      }
+            <Container>
+                <Table striped bordered hover variant="dark">
+                    <thead>
+                        <tr>
+                            <th width="10%">Iteration</th>
+                            <th width="30%">XL</th>
+                            <th width="30%">XM</th>
+                            <th width="30%">XR</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {data.map((element, index)=>{
+                            return  (
+                            <tr key={index}>
+                                <td>{element.iteration}</td>
+                                <td>{element.Xl}</td>
+                                <td>{element.Xm}</td>
+                                <td>{element.Xr}</td>
+                            </tr>)
+                        })}
+                    </tbody>
+                </Table>
+            </Container>
+           
+        );
     }
 
-export default Bisection
+    const error =(xold, xnew)=> Math.abs((xnew-xold)/xnew)*100;
+   
+    const Calbisection = (xl, xr) => {
+        var xm,fXm,fXr,ea,scope;
+        var iter = 0;
+        var MAX = 50;
+        const e = 0.00001;
+        var obj={};
+        do
+        {
+            xm = (xl+xr)/2.0;
+            scope = {
+                x:xr,
+            }
+            fXr = evaluate(Equation, scope)
+
+            scope = {
+                x:xm,
+            }
+            fXm = evaluate(Equation, scope)
+
+            iter ++;
+            if (fXm*fXr > 0)
+            {
+                ea = error(xr, xm);
+                obj = {
+                    iteration:iter,
+                    Xl:xl,
+                    Xm:xm,
+                    Xr:xr
+                }
+                data.push(obj)
+                xr = xm;
+            }
+            else if (fXm*fXr < 0)
+            {
+                ea = error(xl, xm);
+                obj = {
+                    iteration:iter,
+                    Xl:xl,
+                    Xm:xm,
+                    Xr:xr
+                }
+                data.push(obj)
+                xl = xm;
+            }
+        }while(ea>e && iter<MAX)
+        setX(xm)
+    }
+
+    const data =[];
+    const [valueIter, setValueIter] = useState([]);
+    const [valueXl, setValueXl] = useState([]);
+    const [valueXm, setValueXm] = useState([]);
+    const [valueXr, setValueXr] = useState([]);
+     
+   
+    const [html, setHtml] = useState(null);
+    const [Equation,setEquation] = useState("(x^4)-13")
+    const [X,setX] = useState(0)
+    const [XL,setXL] = useState(0)
+    const [XR,setXR] = useState(0)
+
+    const inputEquation = (event) =>{
+        console.log(event.target.value)
+        setEquation(event.target.value)
+    }
+
+    const inputXL = (event) =>{
+        console.log(event.target.value)
+        setXL(event.target.value)
+    }
+
+    const inputXR = (event) =>{
+        console.log(event.target.value)
+        setXR(event.target.value)
+    }
+    //หาราก
+    const calculateRoot = () =>{
+        const xlnum = parseFloat(XL)
+        const xrnum = parseFloat(XR)
+        Calbisection(xlnum,xrnum);
+     
+        setHtml(print());
+           
+        console.log(valueIter)
+        console.log(valueXl)
+    }
+
+    return (
+            <Container>
+                <Form >
+                    <Form.Group className="mb-3">
+                    <Form.Label>Input f(x)</Form.Label>
+                        <input type="text" id="equation" value={Equation} onChange={inputEquation} style={{width:"20%", margin:"0 auto"}} className="form-control"></input>
+                        <Form.Label>Input XL</Form.Label>
+                        <input type="number" id="XL" onChange={inputXL} style={{width:"20%", margin:"0 auto"}} className="form-control"></input>
+                        <Form.Label>Input XR</Form.Label>
+                        <input type="number" id="XR" onChange={inputXR} style={{width:"20%", margin:"0 auto"}} className="form-control"></input>
+                    </Form.Group>
+                    <Button variant="dark" onClick={calculateRoot}>
+                        Calculate
+                    </Button>
+                </Form>
+                <br></br>
+                <h5>Answer = {X.toPrecision(7)}</h5>
+                <Container>
+                {html}
+                </Container>
+               
+            </Container>
+           
+    )
+}
+
+export default Sample
--- Numer/NumerWeb/src/pages/Root/Graphical methods/Graphical.js
+++ Numer/NumerWeb/src/pages/Root/Graphical methods/Graphical.js
@@ -1,11 +1,48 @@
 import React from 'react'
 import Nav from '../../../components/navbar'
+import { Container, Form } from 'react-bootstrap'
+import './Graphical.css'
 function Graphical() {
     return (
-        <div>
+        <div className='body'>
             <Nav />
-            <div>
-                ก๊าบบบบบบบบบบบ
+            <div >
+                <Container>
+                    <Form>
+                        <div className='input'>
+                            <div className='textinp'>
+                                <label>INPUT</label>
+                            </div>
+                            <div className='fx'>
+                                <label for="labfx">
+                                    F(x) :
+                                </label> 
+                                <input type="text" id="inpfx"/>
+                            </div>
+                            <div className='x'>
+                                <label for="labxl">
+                                    XL :
+                                </label> 
+                                <input type="text" id="inpxl"/>
+                                <label for="labxr">
+                                    XR :
+                                </label> 
+                                <input type="text" id="inpxr"/>
+                            </div>
+                            <div className='err'>
+                                <label for="err">
+                                    CHECK ERROR 
+                                </label> <br/>
+                                <input type="text" id="checkerr"/>
+                            </div>
+                        </div>
+                        <div className='bt'>
+                            <button type='button'>
+                                CALCULATE
+                            </button>
+                        </div>
+                    </Form>
+                </Container>
             </div>
         </div>
     )
