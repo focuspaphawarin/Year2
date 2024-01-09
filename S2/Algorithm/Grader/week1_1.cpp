ade196ba9fd0314a14ff7cb587d894caf85a4009
focuspaphawarin <paphawarin.focus@gmail.com>
2024-01-09 13:39:48 +0700

    Algo week1

---
+++ S2/Algorithm/Grader/week1_1.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+    int main()
+{
+    int n = 0, x = 0, a = 0;
+    cin >> n;
+    cin >> x;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a;
+        arr[i] = a;
+
+        // cout << "arr[" << i << "] = " << a << endl;
+    }
+    // cout << "n = " << n << endl;
+    // cout << "x = " << x << endl;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (i == j)
+                continue;
+            if (arr[i] + arr[j] == x)
+            {
+                cout << i << " " << j;
+                return 0;
+            }
+        }
+    }
+
+    cout << "-1";
+}
