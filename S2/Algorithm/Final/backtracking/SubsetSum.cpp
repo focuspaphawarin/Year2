a5a27cdac6c5da84bd641b498d66952363b95a90
focuspaphawarin <paphawarin.focus@gmail.com>
2024-02-17 22:28:20 +0700

    se diagram

---
+++ S2/Algorithm/Final/backtracking/SubsetSum.cpp
@@ -0,0 +1,51 @@
+#include<stdio.h>
+
+int A[] = {25,10,9,2};
+int k = 36;
+
+void print_sol(int x[],int n)
+{
+	int temp = 0;
+	for(int i=1;i<=n;i++)
+	{
+		if(x[i] == 1)
+		{
+			temp += A[i-1];
+		}
+		if(temp == k)
+		{
+			for(int j=1;i<=n;j++)
+			{
+				if (x[j] == 1)
+				{
+					printf("%d ",x[j-1]);
+				}
+			}
+			printf("\n");	
+		}
+	}
+}
+
+void subset1(int x[],int l,int r)
+{
+	if (l == r)
+	{
+		print_sol(x,r);
+		
+	}
+	else
+	{
+		x[l+1] = 1;
+		subset1(x,l+1,r);
+		x[l+1] = 0;
+		subset1(x,l+1,r);	
+	}
+}
+
+int main()
+{
+	int n = 4;
+	int x[n+1];
+	subset1(x,0,n);
+	return 0;
+}
---
+++ S2/Algorithm/Final/backtracking/bt1.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+#include<cstdlib>
+using namespace std ;
+
+void swap(int *a,int *b)
+{
+	int temp = *a;
+	*a = *b ;
+	*b = temp;
+}
+
+void permute(int x[],int start,int end)
+{
+	int flag = 0;
+	if(start == end)
+	{
+		for(int j=1;j<=end;j++)
+		{
+			for(int p=1;p<j;p++)
+			{
+				if(abs(p-j) == abs(x[p]-x[j])||(x[j] == x[p]))
+				{
+					flag = 1;
+				}
+			}
+		}
+		if (flag == 0)
+		{
+			for(int i=1;i<=end;i++)
+			{
+				cout << x[i];
+			}
+			cout << endl;
+			return;
+		}
+	}
+	for (int i=start;i<=end;i++)
+	{
+		swap(&x[start],&x[i]);
+		permute(x,start+1,end);
+		swap(&x[start],&x[i]);
+	}
+}
+
+int main()
+{
+	int n=4;
+	int x[] = {-1,1,2,3,4};
+	
+	permute(x,1,n);
+	
+	return 0 ;
+}
