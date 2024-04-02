a4490a1ae1a740415e6861675061a70467b69f5d
4 <paphawarin.focus@gmail.com>
2024-04-02 23:33:32 +0700

    LAB3/LAB4 Algo

--- S2/Algorithm/Grader/Lab3/Lab3_4.cpp
+++ S2/Algorithm/Grader/Lab3/Lab3_4.cpp
@@ -1,34 +1,70 @@
-#include<iostream>
-using namespace std;
+#include <bits/stdc++.h> 
 
-void MaxSub(int arr[],int L,int R)
-{
-    int maxL , maxR , maxLR ; 
-    if(L<R)
+using namespace std; 
+
+
+int max(int a, int b) 
+{ 
+	return (a > b) ? a : b; 
+} 
+
+int max(int a, int b, int c) 
+{ 
+	return max(max(a, b), c); 
+} 
+
+int maxCrossingSum(int arr[], int l, int m, int h) 
+{ 
+	int sum = 0; 
+	int left_sum = INT_MIN; 
+	for (int i = m; i >= l; i--) 
+    { 
+		sum = sum + arr[i]; 
+		if (sum > left_sum) 
+			left_sum = sum; 
+	} 
+
+	sum = 0; 
+	int right_sum = INT_MIN; 
+	for (int i = m; i <= h; i++) 
+    { 
+		sum = sum + arr[i]; 
+		if (sum > right_sum) 
+			right_sum = sum; 
+	} 
+
+	return max(left_sum + right_sum - arr[m], left_sum, right_sum); 
+} 
+
+
+int maxSubArraySum(int arr[], int l, int h) 
+{ 
+	if (l > h) 
     {
-        int mid = (L+R)/2;
-        maxL = MaxSub(arr,L,mid);
-        maxR = MaxSub(arr,mid+1,R);
-        for (int i=R;i>=0;i--)
-        {
-        }
-        for (int i=L;i<0;i++)
-        {
-        }
+		return INT_MIN; 
     }
-}
-int main()
-{
-    int n;
-    cin >> n;
-    int arr[n];
-    int ML , MR , MLR , L , R , mid;
-    for(int i=0;i<n;i++)
+	if (l == h) 
     {
-        cin >> arr[i];
+		return arr[l]; 
     }
-    // for(int i=0;i<n;i++)
-    // {
-    //     cout << arr[i];
-    // }
-}
+
+	int m = (l + h) / 2; 
+
+	return max(maxSubArraySum(arr, l, m - 1), 
+			maxSubArraySum(arr, m + 1, h), 
+			maxCrossingSum(arr, l, m, h)); 
+} 
+
+int main() 
+{ 
+	int aer;
+	cin >> aer;
+	int arr[aer];
+	for(int i=0;i<aer;i++)
+    {
+		cin >> arr[i];
+	}
+	int n = sizeof(arr) / sizeof(arr[0]); 
+	int max_sum = maxSubArraySum(arr, 0, n - 1); 
+	cout <<  max_sum; 
+} 
